98073f22d5b134c34dfd9db04d48cf058652bcb7
mperusse97 <michael_perusse@college.harvard.edu>
2017-12-07 14:12:21 -0500

    command line args

--- world.py
+++ world.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 import math
 import random
@@ -10,6 +11,16 @@ from clusteringMultiagent import *
 from efficiencyTracker import * 
 
 class World:
+
+    options = {"PASSENGER_PICKUP" : 0,
+           "RANDOM_NAV" : 1,
+           "MAP_ONLY" : 2,
+           "DATA_COLLECTION" : 3,
+           "MAP_AND_PICKUP" : 4,
+           "MULTIAGENT_PICKUP" : 5,
+           "PATH_EFFICIENCY_TRACKER" : 6,
+           "MAP_AND_SHOW_PRM" : 7,
+    }
 
     # run only the multi-agent part of the program
     PASSENGER_PICKUP = 0
@@ -51,7 +62,8 @@ class World:
         ]
 
         ### please just use this I promise everything you have is going to work ###
-        self.mode = World.MULTIAGENT_PICKUP
+        
+        self.mode = World.options[sys.argv[1]]
 
         self.carSize = (20, 20)
         self.kdtreeStart = (0.45 * self.displayWidth, 0.8 * self.displayWidth)
